d8b3710f9ef108fe31128fa17a36321f2342a875
kaewpiyarattong <71188421+kaewpiyarattong@users.noreply.github.com>
2022-03-13 20:24:25 +0700

    Add menuInfo page
    menuInfo page contain name, image, instruction, and ingredients of food *image can not show yet

--- src/views/MenuInfo.vue
+++ src/views/MenuInfo.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="row justify-content-center">
-        <!-- <img
+    <div class="row justify-content-center mx-5 mb-2">        
+        <h3>Name: {{$store.getters.getMenu.menu[0].title}}</h3>
+        <img
              class="img-thumbnail"
-             v-if="$store.getters.getMenu"
-        /> -->
-        {{$store.getters.getMenu.menu}}
-        <!-- <h3>Name: {{menu.title}}</h3> -->
+             v-if="$store.getters.getMenu.menu[0].image_name"
+             :scr="$store.getters.getMenu.menu[0].image_name"
+        />
+        <p><strong>Ingredients: </strong> {{$store.getters.getMenu.menu[0].ingredients}}</p>
+        <p><strong> Instructions: </strong> {{$store.getters.getMenu.menu[0].instructions}}</p>
     </div>
+    <button type="submit" class="btn btn-dark btn-lg btn-block">
+          Add to Bookmark
+        </button>
 </template>
 
 <script>
@@ -19,3 +24,10 @@ export default({
     // }
 })
 </script>
+
+<style scoped>
+img{
+    width: 400px;
+    height: auto;
+}
+</style>
